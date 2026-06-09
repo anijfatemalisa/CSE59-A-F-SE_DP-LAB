fb6e3a05ceccabd62fc74f8d367903489ca8ca84
Lisaa <anijfatemalisa@gmail.com>
2026-06-09 11:16:48 +0600

    Update YouDo6.java

--- Task 2/YouDo6.java
+++ Task 2/YouDo6.java
@@ -1,1 +1,27 @@
 
+// Base class
+class Applet {
+    public void launch() {
+        System.out.println("Applet launched");
+    }
+}
+
+// Derived class
+class GameApplet extends Applet {
+    public void play() {
+        System.out.println("Playing game");
+    }
+}
+
+// Main class
+public class Main6 {
+    public static void main(String[] args) {
+        GameApplet game = new GameApplet();
+
+        // Call inherited method
+        game.launch();
+
+        // Call derived class method
+        game.play();
+    }
+}
