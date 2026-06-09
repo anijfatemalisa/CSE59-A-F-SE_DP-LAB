9f04164958082cf91faa02b33f6c4ac21b5b5b68
Lisaa <anijfatemalisa@gmail.com>
2026-06-09 11:12:47 +0600

    Update YouDo5.java

--- Task 2/YouDo5.java
+++ Task 2/YouDo5.java
@@ -1,1 +1,28 @@
 
+// Printer.java
+public class Printer {
+
+    // Method to print a String
+    public void printData(String text) {
+        System.out.println("Printing text: " + text);
+    }
+
+    // Overloaded method to print an int
+    public void printData(int number) {
+        System.out.println("Printing number: " + number);
+    }
+}
+
+// Main.java
+public class Main5 {
+    public static void main(String[] args) {
+        // Create Printer object
+        Printer printer = new Printer();
+
+        // Call printData with a String
+        printer.printData("Hello, world!");
+
+        // Call printData with an int
+        printer.printData(12345);
+    }
+}
